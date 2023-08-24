c217a89fce2bf2beebed955da9a46f6164288bb9
Socheat KHAUV <pkayjava@gmail.com>
2023-08-24 18:32:04 +0700

    more

---
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+    @RequestMapping(path = "/")
+    public ResponseEntity<String> hello(RequestEntity<String> httpRequest) {
+        return ResponseEntity.ok("hello");
+    }
+
+}
