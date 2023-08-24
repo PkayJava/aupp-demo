600ff4caa163628604ac85a2159593624548f23a
Socheat KHAUV <pkayjava@gmail.com>
2023-08-24 20:06:06 +0700

    Update BasicController.java

--- src/main/java/com/example/demo/controller/BasicController.java
+++ src/main/java/com/example/demo/controller/BasicController.java
@@ -10,7 +10,7 @@ public class BasicController {
 
     @RequestMapping(path = "/")
     public ResponseEntity<String> hello(RequestEntity<String> httpRequest) {
-        return ResponseEntity.ok("hello 3");
+        return ResponseEntity.ok("hello 4");
     }
 
 }
